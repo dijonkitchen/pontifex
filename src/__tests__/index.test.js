44c4ef9bef1a59713fb74e621b9304ee8a9a72f3
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-02-28 17:04:05 -0800

    Test: Check for all cards in deck

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -9,3 +9,9 @@ test('deck exists', () => {
 test('deck has 54 cards', () => {
     expect(deck.length).toEqual(54)
 })
+
+test('deck has all 54 cards', () => {
+    for (let i=1; i <= 54; i++) {
+        expect(deck.includes(i)).toEqual(true)
+    }
+})
