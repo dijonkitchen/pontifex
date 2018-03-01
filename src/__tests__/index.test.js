676a84dd89c4f207a135e64a05ab228ac40e5338
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-02-28 17:15:56 -0800

    Test: Check for conversion of a letter

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,17 +1,19 @@
-import {
-    deck
-} from '../'
+import * as Cipher from '../'
 
 test('deck exists', () => {
-    expect(deck).toBeTruthy()
+    expect(Cipher.deck).toBeTruthy()
 })
 
 test('deck has 54 cards', () => {
-    expect(deck.length).toEqual(54)
+    expect(Cipher.deck.length).toEqual(54)
 })
 
 test('deck has all 54 cards', () => {
     for (let i=1; i <= 54; i++) {
-        expect(deck.includes(i)).toEqual(true)
+        expect(Cipher.deck.includes(i)).toEqual(true)
     }
+})
+
+test('convert letter to number', () => {
+    expect(Cipher.toNumber('a')).toEqual(1)
 })
